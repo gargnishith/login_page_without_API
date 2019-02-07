6d4fce7096da48c3f377ee0670866e1ebe4648ad
garg99 <35557113+gargnishith@users.noreply.github.com>
2019-02-07 20:11:49 +0530

    Update connection.py

--- connection.py
+++ connection.py
@@ -1,5 +1,6 @@
 import sqlite3
 
 conn = sqlite3.connect('da2.db')
+conn.execute("CREATE TABLE un (name TEXT, age TEXT, email TEXT, teacher TEXT)")
 conn.execute('CREATE TABLE users (fname TEXT, lname TEXT, email TEXT, uname TEXT, password TEXT)')
 conn.close()
